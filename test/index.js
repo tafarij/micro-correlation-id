fb82d5802a1936170321332ae70eab53636c0a42
Tafari Johnson <tafarij@users.noreply.github.com>
2022-12-04 14:18:04 -0500

    Revert "feat(breaking-change): swap cls-hooked for asynclocalstorage (#16)"
    This reverts commit eca83fd58dd9b002db5b8975ede89335ec55b15b.

--- test/index.js
+++ test/index.js
@@ -1,89 +1,69 @@
+const micro = require('micro');
 const listen = require('test-listen');
-const t = require('tap');
-const { promisify } = require('util');
-const { createServer } = require('http');
-const fetch = require('node-fetch');
-const { send, serve } = require('micro');
-const { randomUUID } = require('crypto');
-const { correlator, getId } = require('../lib');
+const request = require('request-promise');
+const test = require('ava');
+const sleep = require('then-sleep');
+const {correlator, getId} = require('../lib');
 
-const sleep = promisify(setTimeout);
-
-const service = (req, res) => {
-  t.equal(getId(), req.correlationId());
-  send(res, 200, req.correlationId());
+const testId = 'test-correlation-id';
+const service = req => {
+  return req.correlationId();
 };
 
-const get = (uri, id, header = 'x-correlation-id') => {
-  const headers = id ? { [header]: id } : {};
-  return fetch(uri, { headers }).then((r) => r.text());
-};
-
-const startServer = async (svc, header) => {
-  const server = createServer(serve(correlator(header)(svc)));
+test('set id from incoming request', async t => {
+  const server = micro(correlator()(service));
   const uri = await listen(server);
-  return { uri, server };
-};
 
-t.test('set id from incoming request', async (t) => {
-  const { server, uri } = await startServer(service);
-  const id = randomUUID();
+  const body = await request({
+    uri,
+    headers: {'x-correlation-id': testId}
+  });
 
-  const body = await get(uri, id);
-
-  t.equal(
-    body,
-    id,
-    'req.correlationId() should return id from header of inbound request'
-  );
-
-  server.close();
+  t.is(body, testId, 'req.correlationId() should return id from header of inbound request');
 });
 
-t.test('custom header key', async (t) => {
-  const header = 'x-test-header';
-  const { server, uri } = await startServer(service, header);
-  const id = randomUUID();
+test('custom header key', async t => {
+  const server = micro(correlator('x-test-header')(service));
+  const uri = await listen(server);
 
-  const body = await get(uri, id, header);
+  const body = await request({
+    uri,
+    headers: {'x-test-header': testId}
+  });
 
-  t.equal(
-    body,
-    id,
-    'req.correlationId() should return id from custom header of inbound request'
-  );
-
-  const body2 = await get(uri, randomUUID(), header);
-  t.not(body, body2);
-
-  console.log(body, '----', body2);
-
-  server.close();
+  t.is(body, testId, 'req.correlationId() should return id from custom header of inbound request');
 });
 
-t.test('with await', async (t) => {
-  const id = randomUUID();
-  const svc = async (req, res) => {
-    await sleep(10);
-    send(res, 200, req.correlationId());
+test('req.correlationId() and getId()', async t => {
+  const svc = req => {
+    return {
+      correlationId: req.correlationId(),
+      getId: getId()
+    };
   };
-  const { server, uri } = await startServer(svc);
+  const server = micro(correlator()(svc));
+  const uri = await listen(server);
 
-  const body = await get(uri, id);
+  const body = await request({
+    uri,
+    json: true
+  });
 
-  t.equal(body, id, 'should keep context after await call');
-
-  server.close();
+  t.is(body.correlationId, body.getId, 'req.correlationId() and getId() should return the same id');
 });
 
-t.test('creates correlation id when none passed in header', async () => {
-  const { server, uri } = await startServer(service);
-  const body = await get(uri);
+test('with await', async t => {
+  const svc = async req => {
+    await sleep(10);
+    return req.correlationId();
+  };
+  const server = micro(correlator()(svc));
+  const uri = await listen(server);
 
-  t.ok(
-    body,
-    'a correlation id should be created even if none is passed in the request'
-  );
+  const body = await request({
+    uri,
+    headers: {'x-correlation-id': testId}
+  });
 
-  server.close();
+  t.is(body, testId, 'should keep context after await call');
 });
