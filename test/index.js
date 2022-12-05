3d4b6f4bc555daabea0079a6edff46249a53ff8c
Tafari Johnson <tafari.johnson@gmail.com>
2022-12-04 23:55:55 -0500

    chore: clean up tests

--- test/index.js
+++ test/index.js
@@ -53,11 +53,6 @@ t.test('custom header key', async (t) => {
     'req.correlationId() should return id from custom header of inbound request'
   );
 
-  const body2 = await get(uri, randomUUID(), header);
-  t.not(body, body2);
-
-  console.log(body, '----', body2);
-
   server.close();
 });
 
@@ -72,6 +67,25 @@ t.test('with await', async (t) => {
   const body = await get(uri, id);
 
   t.equal(body, id, 'should keep context after await call');
+
+  server.close();
+});
+
+t.test('multiple requests', async (t) => {
+  const { server, uri } = await startServer(service);
+  const id1 = 'test-id-1';
+  const id2 = 'test-id-2';
+  const id3 = 'test-id-3';
+
+  const [body1, body2, body3] = await Promise.all([
+    get(uri, id1),
+    get(uri, id2),
+    get(uri, id3)
+  ]);
+
+  t.equal(body1, id1);
+  t.equal(body2, id2);
+  t.equal(body3, id3);
 
   server.close();
 });
